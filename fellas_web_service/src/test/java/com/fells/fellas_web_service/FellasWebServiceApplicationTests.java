e6fe2a9b0305b60c87e7a485fa37d4a0a03bcb36
JBall <josephball963@gmail.com>
2024-02-02 12:41:47 -0500

    Created Web Service

---
+++ fellas_web_service/src/test/java/com/fells/fellas_web_service/FellasWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fells.fellas_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FellasWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
