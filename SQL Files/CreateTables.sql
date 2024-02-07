cc22783bfee7088fb090ecb1ba16e129c117c1f2
JBall <josephball963@gmail.com>
2024-02-06 20:53:42 -0500

    wrote some sql

---
+++ SQL Files/CreateTables.sql
@@ -0,0 +1,39 @@
+CREATE SCHEMA "fellas";
+GO 
+
+CREATE TABLE "fellas"."User" (
+    "User_ID" NVARCHAR(8) NOT NULL,
+    "Name" NVARCHAR(255) NOT NULL,
+    "Address" NVARCHAR(255) NOT NULL,
+    "Email" NVARCHAR NOT NULL,
+    "Password" NVARCHAR(20) NOT NULL,
+    PRIMARY KEY ("User_ID")
+);
+
+CREATE TABLE "fellas"."Order" (
+    "Order_ID" VARCHAR(8) NOT NULL,
+    "User_ID" NVARCHAR(8) NOT NULL,
+    "Total_Price" DECIMAL(8, 2) NOT NULL,
+    PRIMARY KEY ("Order_ID"),
+    FOREIGN KEY ("User_ID") REFERENCES "fellas"."User" ("User_ID")
+);
+
+CREATE TABLE "fellas"."Product" (
+    "Product_ID" VARCHAR(8) NOT NULL,
+    "Name" NVARCHAR(255) NOT NULL,
+    "Description" NVARCHAR(255) NOT NULL,
+    "Price" DECIMAL(8, 2) NOT NULL,
+    "Image" BINARY(16) NOT NULL,
+    PRIMARY KEY ("Product_ID")
+);
+
+CREATE TABLE "fellas"."Invoice" (
+    "Invoice_ID" VARCHAR(10) NOT NULL,
+    "Order_ID" VARCHAR(8) NOT NULL,
+    "Product_ID" VARCHAR(8) NOT NULL,
+    "Quantity" BIGINT NOT NULL,
+    "Price" DECIMAL(8, 2) NOT NULL,
+    PRIMARY KEY ("Invoice_ID"),
+    FOREIGN KEY ("Order_ID") REFERENCES "fellas"."Order" ("Order_ID"),
+    FOREIGN KEY ("Product_ID") REFERENCES "fellas"."Product" ("Product_ID")
+);
---
+++ SQL Files/Triggers.sql
@@ -0,0 +1,51 @@
+-- Trigger for "User" table
+GO
+CREATE TRIGGER "set_user_id"
+ON "fellas"."User"
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO "fellas"."User" ("User_ID", "Name", "Address", "Email", "Password")
+    SELECT NEXT VALUE FOR "fellas"."User_Email_Seq", "Name", "Address", "Email", "Password"
+    FROM INSERTED;
+END;
+
+-- Trigger for "Order" table
+GO
+CREATE TRIGGER "set_order_id"
+ON "fellas"."Order"
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO "fellas"."Order" ("Order_ID", "User_ID", "Total_Price")
+    SELECT NEXT VALUE FOR "fellas"."Order_Seq", "User_ID", "Total_Price"
+    FROM INSERTED;
+END;
+
+-- Trigger for "Product" table
+GO
+CREATE TRIGGER "set_product_id"
+ON "fellas"."Product"
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO "fellas"."Product" ("Product_ID", "Name", "Description", "Price", "Image")
+    SELECT NEXT VALUE FOR "fellas"."Product_Seq", "Name", "Description", "Price", "Image"
+    FROM INSERTED;
+END;
+
+-- Trigger for "Invoice" table
+GO
+CREATE TRIGGER "set_invoice_id"
+ON "fellas"."Invoice"
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO "fellas"."Invoice" ("Invoice_ID", "Order_ID", "Product_ID", "Quantity", "Price")
+    SELECT NEXT VALUE FOR "fellas"."Invoice_Seq", "Order_ID", "Product_ID", "Quantity", "Price"
+    FROM INSERTED;
+END;
