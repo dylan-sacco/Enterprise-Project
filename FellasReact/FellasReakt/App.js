07442c395ed6c7d70676e6873a69bfe9c3b5d9f4
AmirFadel <115519463+AmirFadel@users.noreply.github.com>
2024-02-07 02:12:00 -0500

    Amir Commit
    Installed the stuff for react nav (Mac people need to do one more thing) and wrapped the starter expo function inside of a nav container like the docs said to.

--- FellasReact/FellasReakt/App.js
+++ FellasReact/FellasReakt/App.js
@@ -1,15 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
   return (
+    //doc said we only need one nav container and all our interface code needs to be in it - Amir
+  <NavigationContainer>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
+
+//StyleSheets
 const styles = StyleSheet.create({
   container: {
     flex: 1,
