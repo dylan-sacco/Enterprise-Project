cb7524c0ff234ac4b81e09781066b0498ba16831
AmirFadel <115519463+AmirFadel@users.noreply.github.com>
2024-02-07 18:37:47 -0500

    Amir Commit
    Was fiddlin and fuckin with react needed to test the app on laptop

--- FellasReact/FellasReakt/App.js
+++ FellasReact/FellasReakt/App.js
@@ -1,17 +1,55 @@
-import * as React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
 
-export default function App() {
+import * as React from 'react';
+import * as ReactNative from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+
+//HOME PAGE
+function Home({navigation}){
+  <View style={styles.container}>
+      <text>Home Page Test</text>
+      <Button title="Go to Shopping" onPress={() => navigation.navigate('Shopping')}/>
+      <Button title="Go to Details" onPress={() => navigation.navigate('Cart')}/>
+
+  </View>
+}
+//SHOPPING PAGE
+function Shopping(){
+  <View style={styles.container}>
+      <text>Shopping Page Test</text>
+  </View>
+}
+//CART PAGE
+function Cart(){
+  <View style={styles.container}>
+      <text>Cart Page Test</text>
+  </View>
+}
+
+
+
+//this is for declaring the stack navigator
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
     //doc said we only need one nav container and all our interface code needs to be in it - Amir
   <NavigationContainer>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-    </NavigationContainer>
+   <Stack.Navigator>
+
+      <Stack.Screen name="Home" component={Home} options={{title: 'Home'}}/>
+
+      <Stack.Screen name="Shopping" component={Shopping} options={{title: 'Shopping'}}/>
+
+      <Stack.Screen name="Cart" component={Cart} options={{title: 'Cart'}}/>
+
+   </Stack.Navigator> 
+  </NavigationContainer>
   );
 }
+export default App;
 
 
 //StyleSheets
