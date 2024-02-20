a06c1e7e6ba620026543687a99d9c37ee68cda0e
Dylan Sacco <Dms64@students.ptcollege.edu>
2024-02-20 14:52:39 -0500

    Working Auth flow stack

--- yarnCreateExpo/App.js
+++ yarnCreateExpo/App.js
@@ -1,82 +1,103 @@
-// system imports
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import * as Linking from 'expo-linking';
-import * as SecureStore from 'expo-secure-store';
-import  {  StyleSheet, Text, View, Image, Platform, ScrollView, Button, TouchableOpacity, TextInput
-  } from 'react-native';
+// system imports hi dylan :3
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import * as Linking from "expo-linking";
+import * as SecureStore from "expo-secure-store";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Platform,
+  ScrollView,
+  Button,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 // navigation imports
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useNavigation, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  useNavigation,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
 
 // custom components imports
-import {ProductBubble, CheckBox} from './Components.js';
+import { ProductBubble, CheckBox } from "./Components.js";
 
 // firebase imports
-import './firebaseConfig.js';
-import {app, auth} from './firebaseConfig.js';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithCustomToken, createCustomToken} from "firebase/auth";
+import "./firebaseConfig.js";
+import { app, auth } from "./firebaseConfig.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithCustomToken,
+  createCustomToken,
+} from "firebase/auth";
 
 function SignIn({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const { signIn } = React.useContext(AuthContext);
-  function setErrorCodes(error){
+  function setErrorCodes(error) {
     const { code, message } = error;
-    let formattedCode = code !== null ? code.replace('auth/', '').replace(/-/g, ' ').toUpperCase() : '';
+    let formattedCode =
+      code !== null
+        ? code.replace("auth/", "").replace(/-/g, " ").toUpperCase()
+        : "";
     setEmailError(formattedCode);
   }
   const signin = async () => {
     signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed in 
-          const user = userCredential.user;
-          signIn({token: user.stsTokenManager.accessToken});
-        })
-        .catch((error) => {
-          setErrorCodes(error);
-        });
-  }
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        signIn({ token: user.stsTokenManager.accessToken });
+      })
+      .catch((error) => {
+        setErrorCodes(error);
+      });
+  };
   const signup = async () => {
     // empty for now
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        signIn({token: user.stsTokenManager.accessToken});
+        signIn({ token: user.stsTokenManager.accessToken });
       })
       .catch((error) => {
         setErrorCodes(error);
       });
-      
-  }
+  };
 
   return (
-    <View style={{padding:20, maxWidth:800, }}>
+    <View style={{ padding: 20, maxWidth: 800 }}>
       <Text>Sign In</Text>
       <Text>Email</Text>
-      
+
       <TextInput
-        autoComplete='email'
+        autoComplete="email"
         placeholder="Email"
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={text => setEmail(text)}
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={(text) => setEmail(text)}
         value={email}
       />
       <Text>Password</Text>
       <TextInput
         placeholder="Password"
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
         secureTextEntry={true}
-        onChangeText={text => setPassword(text)}
+        onChangeText={(text) => setPassword(text)}
         value={password}
       />
-      {(emailError) ? <Text style={{color: 'red'}}>{emailError}</Text> : null}
+      {emailError ? <Text style={{ color: "red" }}>{emailError}</Text> : null}
       <Button title="Log In" onPress={signin} />
       <Button title="Sign Up" onPress={signup} />
     </View>
@@ -86,103 +107,150 @@ function SignIn({ navigation }) {
 function HomeScreen({ navigation }) {
   return (
     <ScrollView>
-      <Text style={{fontSize:30, textAlign:'center', width:'100%',}}>Welcome to{"\n"}Fellas Clothing CO</Text>
+      <Text style={{ fontSize: 30, textAlign: "center", width: "100%" }}>
+        Welcome to{"\n"}Fellas Clothing CO
+      </Text>
       <Text>Home Screen</Text>
       <Button
         title="Go to Products"
         onPress={() => {
           // navigation.navigate('Products', {productId: 'ca724'}); // use navigate to go to an existing screen
-          navigation.push('Products', {productId: 'ca724'}); // use push to create a new screen
+          navigation.push("Products", { productId: "ca724" }); // use push to create a new screen
         }}
-        >
-      </Button>
+      ></Button>
       <Button
         title="Print user to Logs"
         onPress={() => {
           console.log(auth.currentUser);
           console.log("User is signed " + isSignedIn);
         }}
+      ></Button>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          // justifyContent: 'center'
+          paddingTop: 20,
+          alignSelf: "center",
+          // borderColor: 'black',
+          // borderWidth: 2,
+          margin: 10,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "left",
+            width: "100%",
+            alignSelf: "center",
+
+            ...Platform.select({
+              web: {
+                maxWidth: 1000,
+              },
+            }),
+          }}
         >
-      </Button>
-      <View style={
-      { 
-        flex: 1, 
-        alignItems: 'center', 
-        // justifyContent: 'center' 
-        paddingTop: 20,
-        alignSelf: 'center',
-        // borderColor: 'black',
-        // borderWidth: 2,
-        margin: 10,
+          <ProductBubble
+            image="https://media.gucci.com/style/DarkGray_Center_0_0_980x980/1584562506/628717_H9H80_1074_001_100_0000_Light-Mens-Gucci-Off-The-Grid-high-top-sneaker.jpg"
+            price="23.75"
+            name="Shoe"
+          />
 
-        
-    }}>
-      
+          <ProductBubble
+            image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQFBGrDfewGS86JSoe8g4ywc4KtHtaomDauwj2HUTkPTZOJBj0tea2cH09Dd3hH_IMwcQSbI8Nmeh7bum0KdvfiyJki0yKWZVr5-F12CCIWWz8Z5SvsRUKh&usqp=CAc"
+            price="23.75"
+            name="Belt"
+          />
 
-      <View style={{
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'left',
-        width: '100%',
-        alignSelf: 'center',
-        
-        ...Platform.select({
-          web: {
-            maxWidth:1000
-          }
-        })
-      }}>
-      <ProductBubble image="https://media.gucci.com/style/DarkGray_Center_0_0_980x980/1584562506/628717_H9H80_1074_001_100_0000_Light-Mens-Gucci-Off-The-Grid-high-top-sneaker.jpg" price="23.75" name="Shoe"/>
+          <ProductBubble
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg"
+            price="23.75"
+            name="Corn Dog"
+          />
 
-      <ProductBubble image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQFBGrDfewGS86JSoe8g4ywc4KtHtaomDauwj2HUTkPTZOJBj0tea2cH09Dd3hH_IMwcQSbI8Nmeh7bum0KdvfiyJki0yKWZVr5-F12CCIWWz8Z5SvsRUKh&usqp=CAc" price="23.75" name="Belt"/>
+          <ProductBubble
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg"
+            price="23.75"
+            name="Corn Dog"
+          />
 
-      <ProductBubble image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg" price="23.75" name="Corn Dog"/>
+          <ProductBubble
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg"
+            price="23.75"
+            name="Corn Dog"
+          />
 
-      <ProductBubble image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg" price="23.75" name="Corn Dog"/>
+          <ProductBubble
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg"
+            price="23.75"
+            name="Corn Dog"
+          />
 
-      <ProductBubble image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg" price="23.75" name="Corn Dog"/>
+          <ProductBubble
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg"
+            price="23.75"
+            name="Corn Dog"
+          />
 
-      <ProductBubble image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg" price="23.75" name="Corn Dog"/>
+          <ProductBubble
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg"
+            price="23.75"
+            name="Corn Dog"
+          />
 
-      <ProductBubble image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg" price="23.75" name="Corn Dog"/>
+          <ProductBubble
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg"
+            price="23.75"
+            name="Corn Dog"
+          />
 
-      <ProductBubble image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg" price="23.75" name="Corn Dog"/>
-
-      <ProductBubble image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg" price="23.75" name="Corn Dog"/>
-
-      <ProductBubble image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg" price="23.75" name="Corn Dog"/>
+          <ProductBubble
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/CornDog.jpg/1200px-CornDog.jpg"
+            price="23.75"
+            name="Corn Dog"
+          />
+        </View>
       </View>
-    </View>
     </ScrollView>
   );
 }
 
 function ProductScreen({ route, navigation }) {
-  const { productId, url, price, productName} = route.params;
+  const { productId, url, price, productName } = route.params;
   console.log(route.params);
   const onPress = () => {
-    console.log('Add to cart');
-  }
+    console.log("Add to cart");
+  };
   return (
-    <View style={
-      { 
-        flex: 1, 
-        alignItems: 'center', 
-        // justifyContent: 'center' 
-        paddingTop: 50
-    }}>
-    
-        {url ?(
-          <Image source={{uri: url}} style={{height:200, width:200}}/>
-        ) :
-          <Image source={require('./assets/Square_200x200.png')}/>
-        }
-      <Text style={{fontSize:30, textAlign:'center'}}>{productName || "none"}</Text>
-      <Text>${price|| "0.00"}</Text>
-      <TouchableOpacity onPress={onPress} style={{backgroundColor: 'rgba(173, 216, 230, 0.8)', padding: 10, borderRadius:9 }}>
-        <Text style={{fontSize:20}}>Add To Cart</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        // justifyContent: 'center'
+        paddingTop: 50,
+      }}
+    >
+      {url ? (
+        <Image source={{ uri: url }} style={{ height: 200, width: 200 }} />
+      ) : (
+        <Image source={require("./assets/Square_200x200.png")} />
+      )}
+      <Text style={{ fontSize: 30, textAlign: "center" }}>
+        {productName || "none"}
+      </Text>
+      <Text>${price || "0.00"}</Text>
+      <TouchableOpacity
+        onPress={onPress}
+        style={{
+          backgroundColor: "rgba(173, 216, 230, 0.8)",
+          padding: 10,
+          borderRadius: 9,
+        }}
+      >
+        <Text style={{ fontSize: 20 }}>Add To Cart</Text>
       </TouchableOpacity>
-
     </View>
   );
 }
@@ -195,13 +263,16 @@ export default function App() {
   // const navigation = useNavigation();
   const [state, dispatch] = React.useReducer(
     (prevState, action) => {
-      switch (action.type) { 
-        case 'RESTORE_TOKEN': return {...prevState, userToken: action.token, isLoading: false, };
-        case 'SIGN_IN': return { ...prevState, isSignout: false, userToken: action.token, };
-        case 'SIGN_OUT': return { ...prevState, isSignout: true, userToken: null,};
+      switch (action.type) {
+        case "RESTORE_TOKEN":
+          return { ...prevState, userToken: action.token, isLoading: false };
+        case "SIGN_IN":
+          return { ...prevState, isSignout: false, userToken: action.token };
+        case "SIGN_OUT":
+          return { ...prevState, isSignout: true, userToken: null };
       }
     },
-    { isLoading: true, isSignout: false, userToken: null, }
+    { isLoading: true, isSignout: false, userToken: null },
   );
 
   React.useEffect(() => {
@@ -210,11 +281,11 @@ export default function App() {
       let userToken;
 
       try {
-        userToken = await SecureStore.getItemAsync('userToken');
+        userToken = await SecureStore.getItemAsync("userToken");
       } catch (e) {
         // Restoring token failed
         console.log(e);
-        console.log('Failed to restore token');
+        console.log("Failed to restore token");
       }
 
       // After restoring token, we may need to validate it in production apps
@@ -233,31 +304,29 @@ export default function App() {
       signIn: async (data) => {
         console.log(data);
         // In a production app, we need to send some data (usually username, password) to server and get a token
-        
+
         // We will also need to handle errors if sign in failed
         // After getting token, we need to persist the token using `SecureStore`
         // In the example, we'll use a dummy token
-        dispatch({ type: 'SIGN_IN', token: data.token });
+        dispatch({ type: "SIGN_IN", token: data.token });
       },
-      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signOut: () => dispatch({ type: "SIGN_OUT" }),
       signUp: async (data) => {
         // In a production app, we need to send user data to server and get a token
         // We will also need to handle errors if sign up failed
         // After getting token, we need to persist the token using `SecureStore`
         // In the example, we'll use a dummy token
 
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+        dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
       },
     }),
-    []
+    [],
   );
 
   return (
-
     <AuthContext.Provider value={authContext}>
       <NavigationContainer>
         <Stack.Navigator>
-
           {state.userToken == null ? (
             <>
               <Stack.Screen name="SignIn" component={SignIn} />
@@ -266,7 +335,11 @@ export default function App() {
           ) : (
             <>
               <Stack.Screen name="Home" component={HomeScreen} />
-              <Stack.Screen name="Products" component={ProductScreen} initialParams={{productId:'ca724'}} />
+              <Stack.Screen
+                name="Products"
+                component={ProductScreen}
+                initialParams={{ productId: "ca724" }}
+              />
               {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
             </>
           )}
@@ -279,8 +352,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
     // justifyContent: 'center',
   },
 });
+//hi
--- yarnCreateExpo/Components.js
+++ yarnCreateExpo/Components.js
@@ -1,135 +1,135 @@
 // Components.js
-import React, { useState } from 'react';
-import { Platform, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 // https://reactnavigation.org/docs/connecting-navigation-prop
 // allows me to not have to pass in navigation as a prop every time i use the damn object
-import { useNavigation } from '@react-navigation/native';
-
+import { useNavigation } from "@react-navigation/native";
 
 const ProductBubbleStyles = StyleSheet.create({
-    touch:{
-        overflow: 'hidden',
-        width: '100%',
-        height: '100%',
-        borderRadius:20,
-
-    },
-    container: {
-        ...Platform.select({
-            ios: {
-            minWidth: 150,
-            },
-            android: {
-            minWidth: 150,
-            },
-            web: {
-            minWidth: 300,
-            }
-        }),
-        // width: '100%',
-        height: 200,
-        borderRadius:20,
-        margin: 10,
-        flex: 1,
-        shadowColor: 'black',
-        shadowOffset: { width: 4, height: 6 },
-        shadowOpacity: 0.5,
-        shadowRadius: 2,
-        elevation: 5,
-        backgroundColor: 'rgba(0,0,0,0.2)',
-    },
-    text: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 20,
-        textAlign: 'left',
-        padding:2,
-
-    },
-    textContainer: {
-        backgroundColor: 'rgba(0,0,0,0.4)',
-        bottom: 0,
-        position: 'absolute',
-        width: '100%',
-        borderRadius: 9,
-        padding: 9,
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'fill',
-        resizeMode: 'cover',
-        // resizeMode: 'contain',
-        // resizeMode: 'stretch',
-        // backgroundColor: 'rgba(0,0,0,0.2)',
-    }
-  
+  touch: {
+    overflow: "hidden",
+    width: "100%",
+    height: "100%",
+    borderRadius: 20,
+  },
+  container: {
+    ...Platform.select({
+      ios: {
+        minWidth: 150,
+      },
+      android: {
+        minWidth: 150,
+      },
+      web: {
+        minWidth: 300,
+      },
+    }),
+    // width: '100%',
+    height: 200,
+    borderRadius: 20,
+    margin: 10,
+    flex: 1,
+    shadowColor: "black",
+    shadowOffset: { width: 4, height: 6 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 5,
+    backgroundColor: "rgba(0,0,0,0.2)",
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "left",
+    padding: 2,
+  },
+  textContainer: {
+    backgroundColor: "rgba(0,0,0,0.4)",
+    bottom: 0,
+    position: "absolute",
+    width: "100%",
+    borderRadius: 9,
+    padding: 9,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "fill",
+    resizeMode: "cover",
+    // resizeMode: 'contain',
+    // resizeMode: 'stretch',
+    // backgroundColor: 'rgba(0,0,0,0.2)',
+  },
 });
 const ProductBubble = (props) => {
-    const navigation = useNavigation(); 
-    function onPress() {
+  const navigation = useNavigation();
+  function onPress() {
     //   console.log('Pressed ', props.name, ' for $', props.price, '!');
     //   console.log("this is where we would navigate to the product page");
-        navigation.push(
-        'Products', {
-            productId: 'ca724', 
-            productName: props.name, 
-            price: props.price, 
-            url: props.image
-        }); // use push to create a new screen
-    }
-    return(
+    navigation.push("Products", {
+      productId: "ca724",
+      productName: props.name,
+      price: props.price,
+      url: props.image,
+    }); // use push to create a new screen
+  }
+  return (
     <View style={ProductBubbleStyles.container}>
-    <TouchableOpacity style={ProductBubbleStyles.touch}
-    onPress={onPress}>
-    <Image style={ProductBubbleStyles.image} source={{uri: props.image} /*|| require('./assets/Square_200x200.png') */} />
-    <View style={ProductBubbleStyles.textContainer}>
-        <Text style={ProductBubbleStyles.text}>
-        {props.name}
-        </Text>
-        <Text style={ProductBubbleStyles.text}>
-        ${props.price} 
-        </Text>
+      <TouchableOpacity style={ProductBubbleStyles.touch} onPress={onPress}>
+        <Image
+          style={ProductBubbleStyles.image}
+          source={
+            { uri: props.image } /*|| require('./assets/Square_200x200.png') */
+          }
+        />
+        <View style={ProductBubbleStyles.textContainer}>
+          <Text style={ProductBubbleStyles.text}>{props.name}</Text>
+          <Text style={ProductBubbleStyles.text}>${props.price}</Text>
+        </View>
+      </TouchableOpacity>
     </View>
-    </TouchableOpacity>
-    </View>
-      
-    );
+  );
 };
-
 
 const CheckBoxStyles = StyleSheet.create({
-    container: {
-        height: 50,
-        flexDirection: 'row',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        color: 'black',
-        fontWeight: 'bold',
-    },
-    checkBox: {
-        width: 30,
-        height: 30,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    height: 50,
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "black",
+    fontWeight: "bold",
+  },
+  checkBox: {
+    width: 30,
+    height: 30,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 const CheckBox = (props) => {
-    const [isChecked, setIsChecked] = useState(false);
-    return (
-        <View style={CheckBoxStyles.container}>
-            <TouchableOpacity style={CheckBoxStyles.CheckBox} > 
-                <Text style={CheckBoxStyles.text}>X</Text>
-            </TouchableOpacity>
-            <Text style={CheckBoxStyles.text}>Checkbox</Text>
-        </View>
-    );
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <View style={CheckBoxStyles.container}>
+      <TouchableOpacity style={CheckBoxStyles.CheckBox}>
+        <Text style={CheckBoxStyles.text}>X</Text>
+      </TouchableOpacity>
+      <Text style={CheckBoxStyles.text}>Checkbox</Text>
+    </View>
+  );
 };
 
-export { ProductBubble, CheckBox }; 
+export { ProductBubble, CheckBox };
