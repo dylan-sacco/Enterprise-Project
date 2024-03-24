c11c52b319ad0bf5c9d96046a4397c925fff5d77
Dylan Sacco <Dms64@students.ptcollege.edu>
2024-03-23 21:51:48 -0400

    Updated cart page.
    I still need to make the cart get items from a list, because the list will be aquired by the databse.

--- yarnCreateExpo/pages/cartPage.js
+++ yarnCreateExpo/pages/cartPage.js
@@ -1,14 +1,87 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#e5e5e5",
+    padding: 10
+  },
+  padding: {
+    padding: 5,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "#f77",
+    borderRadius: 7,
+    flex: 1,
+    alignItems: "center",
+  }
+});
+
+const CartItem = (props) => {
+
+  return (
+    <View>
+      {/* Image and text */}
+      <View style={style.container}>
+        <Text>Image</Text>
+        <View>
+          <View style={style.padding}>
+            <Text>Product ID:</Text>
+            <Text>{props.productID}</Text>
+          </View>
+          <View style={style.padding}>
+            <Text>Product Name: </Text>
+            <Text>Name</Text>
+          </View>
+          <View style={style.padding}>
+            <Text>price: </Text>
+            <Text>$0.00</Text>
+          </View>
+          <View style={style.padding}>
+            <Text>Quantity: </Text>
+            <Text>0</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* Buttons */}
+      <View style={style.container}>
+        <TouchableOpacity style={style.button}>
+          <Text>Remove</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 function Cart({ navigation }) {
-    return (
+  return (
+    <View style={{justifyContent: "space-between", flex: 1}}>
       <ScrollView>
-        <Text style={{ fontSize: 30, textAlign: "center", width: "100%" }}>
-          Cart
-        </Text>
+        <CartItem productID="1" />
+        <CartItem productID="2" />
+        <CartItem productID="3" />
+        <CartItem productID="4" />
+        <CartItem productID="5" />
       </ScrollView>
-    );
-  }
+      
 
-  export default Cart;
+      <View>
+        <TouchableOpacity style={[style.button, {flex:0, margin:10, backgroundColor:"lightblue"}]}>
+          <Text>Checkout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export default Cart;
